929b6ec8a2f9a41c30ec72c2a16b1d465c9b133b
tarunchaudharydev <tarunchaudhary.dev@gmail.com>
2026-03-23 07:52:13 +0530

    update types

---
+++ content/nav/navLinks.ts
@@ -0,0 +1,9 @@
+import { NavLink } from "@/types/navbar/navbar";
+
+export const navLinks: NavLink[] =[
+  {
+    href: "/",
+    label: "Home",
+  },
+
+];
---
+++ content/sponsors/sponsors.ts
@@ -0,0 +1,8 @@
+import { Sponsor } from "@/types/sponsors/sponsors";
+
+export const sponsors: Sponsor[] =[
+  {
+    name: "Adani",
+    logo: "/sponsors/adani.png",
+  },
+];
